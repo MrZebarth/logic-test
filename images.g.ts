8f1de465c690634cdace9fd37d826b4bdc63869e
Kris Zebarth <kris.zebarth@ocsb.ca>
2023-11-28 10:58:08 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,177 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mario_run_1":return img`
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . e e e 4 4 e 4 4 . . . . 
+. . . e 4 e 4 4 4 e 4 4 4 4 . . 
+. . . e 4 e e 4 4 4 e 4 4 4 4 . 
+. . . e e 4 4 4 4 e e e e e . . 
+. . . . . 4 4 4 4 4 4 4 4 . . . 
+. . . . e e e e 2 e e 4 4 . . . 
+. . . 4 e e e e e e 4 4 4 4 . . 
+. . 4 4 2 e e e e e 4 4 4 . . . 
+. . e e 2 2 2 2 2 2 2 2 . . . . 
+. . e 2 2 2 2 2 2 2 2 2 . . . . 
+. e e 2 2 2 2 2 2 2 2 . . . . . 
+. e e . . . e e e e . . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "mario_run_2":return img`
+. . . . . 2 2 2 2 2 e . . . . . 
+. . . . 2 2 2 2 2 2 2 2 2 e . . 
+. . . . e e e 4 4 e 4 e . . . . 
+. . . e 4 e 4 4 4 e 4 4 4 e . . 
+. . . e 4 e e 4 4 4 e 4 4 4 e . 
+. . . e 4 4 4 4 4 e e e e e . . 
+. . . . . 4 4 4 4 4 4 4 e . . . 
+. . . . e e 2 e e e e . . . . . 
+. . . e e e e 2 2 e e e . . . . 
+. . . e e e 2 2 4 2 2 4 e . . . 
+. . . e e e e 2 2 2 2 2 e . . . 
+. . . 2 e 4 4 4 4 2 2 2 e . . . 
+. . . . 2 4 4 4 2 2 2 e . . . . 
+. . . . . 2 2 2 e e e e . . . . 
+. . . . . e e e e e e e e . . . 
+. . . . . e e e e e . . . . . . 
+`;
+            case "image3":
+            case "mario_run_3":return img`
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . e e e 4 4 e 4 4 . . . . 
+. . . e 4 e 4 4 4 e 4 4 4 4 . . 
+. . . e 4 e e 4 4 4 e 4 4 4 4 . 
+. . . e e 4 4 4 4 e e e e e . . 
+. . . . . 4 4 4 4 4 4 4 4 . . . 
+. . e e e e 2 2 e e e . . . . . 
+4 4 e e e e 2 2 2 e e e 4 4 4 4 
+4 4 4 4 e e 2 4 2 2 2 e e 4 4 4 
+4 4 4 . 2 2 2 2 2 2 2 2 . e e . 
+. . . 2 2 2 2 2 2 2 2 2 e e e . 
+. . 2 2 2 2 2 2 2 2 2 2 e e e . 
+. e e 2 2 2 2 . . 2 2 2 e e e . 
+. e e e e . . . . . . . . . . . 
+. . e e e e . . . . . . . . . . 
+`;
+            case "image4":
+            case "mario_run_4":return img`
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . 2 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . 4 4 e 4 4 e e e . . . . 
+. . 4 4 4 4 e 4 4 4 e 4 e . . . 
+. 4 4 4 4 e 4 4 4 e e 4 e . . . 
+. . e e e e e 4 4 4 4 e e . . . 
+. . . 4 4 4 4 4 4 4 4 . . . . . 
+. . . 4 4 e e 2 e e e e . . . . 
+. . 4 4 4 4 e e e e e e 4 . . . 
+. . . 4 4 4 e e e e e 2 4 4 . . 
+. . . . 2 2 2 2 2 2 2 2 e e . . 
+. . . . 2 2 2 2 2 2 2 2 2 e . . 
+. . . . . 2 2 2 2 2 2 2 2 e e . 
+. . . . . . e e e e . . . e e . 
+. . . . . e e e e e . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "mario_run_5":return img`
+. . . . . e 2 2 2 2 2 . . . . . 
+. . e 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . e 4 e 4 4 e e e . . . . 
+. . e 4 4 4 e 4 4 4 e 4 e . . . 
+. e 4 4 4 e 4 4 4 e e 4 e . . . 
+. . e e e e e 4 4 4 4 4 e . . . 
+. . . e 4 4 4 4 4 4 4 . . . . . 
+. . . . . e e e e 2 e e . . . . 
+. . . . e e e 2 2 e e e e . . . 
+. . . e 4 2 2 4 2 2 e e e . . . 
+. . . e 2 2 2 2 2 e e e e . . . 
+. . . e 2 2 2 4 4 4 4 e 2 . . . 
+. . . . e 2 2 2 4 4 4 2 . . . . 
+. . . . e e e e 2 2 2 . . . . . 
+. . . e e e e e e e e . . . . . 
+. . . . . . e e e e e . . . . . 
+`;
+            case "image6":
+            case "mario_run_6":return img`
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . 2 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . 4 4 e 4 4 e e e . . . . 
+. . 4 4 4 4 e 4 4 4 e 4 e . . . 
+. 4 4 4 4 e 4 4 4 e e 4 e . . . 
+. . e e e e e 4 4 4 4 e e . . . 
+. . . 4 4 4 4 4 4 4 4 . . . . . 
+. . . . . e e e 2 2 e e e e . . 
+4 4 4 4 e e e 2 2 2 e e e e 4 4 
+4 4 4 e e 2 2 2 4 2 e e 4 4 4 4 
+. e e . 2 2 2 2 2 2 2 2 . 4 4 4 
+. e e e 2 2 2 2 2 2 2 2 2 . . . 
+. e e e 2 2 2 2 2 2 2 2 2 2 . . 
+. e e e 2 2 2 . . 2 2 2 2 e e . 
+. . . . . . . . . . . e e e e . 
+. . . . . . . . . . e e e e . . 
+`;
+            case "image7":
+            case "Mario Idle":return img`
+. . . . . 2 2 2 2 2 e . . . . . 
+. . . . 2 2 2 2 2 2 2 2 2 e . . 
+. . . . e e e 4 4 e 4 e . . . . 
+. . . e 4 e 4 4 4 e 4 4 4 e . . 
+. . . e 4 e e 4 4 4 e 4 4 4 e . 
+. . . e e 4 4 4 4 e e e e e . . 
+. . . . . 4 4 4 4 4 4 4 e . . . 
+. . . . e e 2 e e e e . . . . . 
+. . . e e e 2 e 4 2 e e e e . . 
+. . 4 e e 4 2 2 2 2 e e 4 4 e . 
+. . 4 4 4 2 4 2 2 4 2 4 4 4 e . 
+. . 4 4 4 2 2 2 2 2 2 4 4 4 e . 
+. . 4 4 2 2 2 2 2 2 2 2 4 4 e . 
+. . . . 2 2 2 e . 2 2 2 e . . . 
+. . . e e e e . . . e e e e . . 
+. . e e e e e . . . e e e e e . 
+`;
+            case "image8":
+            case "Mario Idle2":return img`
+. . . . . e 2 2 2 2 2 . . . . . 
+. . e 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . e 4 e 4 4 e e e . . . . 
+. . e 4 4 4 e 4 4 4 e 4 e . . . 
+. e 4 4 4 e 4 4 4 e e 4 e . . . 
+. . e e e e e 4 4 4 4 e e . . . 
+. . . e 4 4 4 4 4 4 4 . . . . . 
+. . . . . e e e e 2 e e . . . . 
+. . e e e e 2 4 e 2 e e e . . . 
+. e 4 4 e e 2 2 2 2 4 e e 4 . . 
+. e 4 4 4 2 4 2 2 4 2 4 4 4 . . 
+. e 4 4 4 2 2 2 2 2 2 4 4 4 . . 
+. e 4 4 2 2 2 2 2 2 2 2 4 4 . . 
+. . . e 2 2 2 . e 2 2 2 . . . . 
+. . e e e e . . . e e e e . . . 
+. e e e e e . . . e e e e e . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,67 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping,
+    LWalk,
+    RWalk,
+    LIdle,
+    RIdle
+}
+function Setup_Animations () {
+    Left_Walk()
+    Right_Walk()
+    Left_Idle()
+    Right_Idle()
+}
+function Right_Idle () {
+    rightIdleAnim = animation.createAnimation(ActionKind.RIdle, 1000)
+    rightIdleAnim.addAnimationFrame(assets.image`Mario Idle`)
+    animation.attachAnimation(mySprite, rightIdleAnim)
+}
+function Right_Walk () {
+    rightWalkAnim = animation.createAnimation(ActionKind.RWalk, 100)
+    rightWalkAnim.addAnimationFrame(assets.image`mario_run_1`)
+    rightWalkAnim.addAnimationFrame(assets.image`mario_run_2`)
+    rightWalkAnim.addAnimationFrame(assets.image`mario_run_3`)
+    rightWalkAnim.addAnimationFrame(assets.image`mario_run_2`)
+    animation.attachAnimation(mySprite, rightWalkAnim)
+}
+function Left_Walk () {
+    leftWalkAnim = animation.createAnimation(ActionKind.LWalk, 100)
+    leftWalkAnim.addAnimationFrame(assets.image`mario_run_4`)
+    leftWalkAnim.addAnimationFrame(assets.image`mario_run_5`)
+    leftWalkAnim.addAnimationFrame(assets.image`mario_run_6`)
+    leftWalkAnim.addAnimationFrame(assets.image`mario_run_5`)
+    animation.attachAnimation(mySprite, leftWalkAnim)
+}
+function Left_Idle () {
+    leftIdleAnim = animation.createAnimation(ActionKind.LIdle, 100)
+    leftIdleAnim.addAnimationFrame(assets.image`Mario Idle2`)
+    animation.attachAnimation(mySprite, leftIdleAnim)
+}
+let leftIdleAnim: animation.Animation = null
+let leftWalkAnim: animation.Animation = null
+let rightWalkAnim: animation.Animation = null
+let rightIdleAnim: animation.Animation = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Setup_Animations()
+animation.setAction(mySprite, ActionKind.RIdle)
+controller.moveSprite(mySprite, 100, 0)
